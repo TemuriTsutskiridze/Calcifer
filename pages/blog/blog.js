1b98e0bec5086e6bd86285d94270798faaaa739f
tikosaghinadze <teasaghinadze20@gmail.com>
2024-01-19 02:28:07 +0400

    add in blog.html html tags

--- pages/blog/blog.js
+++ pages/blog/blog.js
@@ -8,6 +8,26 @@ async function loadData(id) {
   const data = await response.json();
   const post = data.find((item) => item.id == id); //წამოიღებს მთლიან ობიექტს
 }
+const fullPostContainer = document.getElementById("full-post-container");
+
+const fullPostElement = document.createElement("div");
+fullPostElement.classList.add("blog-post");
+
+fullPostElement.innerHTML = `
+    <img src="${blogId.image}" alt="Blog Image" class="post-image">
+    <div class="post-details">
+    <div class="personal-info">
+      <h3 class="author">${post.author}</h3>
+      <h4 class="publish-date">${post.publish_date}</h4>
+      </div>
+      <h2 class="title">${post.title}</h2>
+      <div class="display-categories">${post.categories
+        .map((category) => displayCategories(category).outerHTML)
+        .join("")}</div>
+      <p class="description">${post.description}</p>
+  `;
+
+fullPostContainer.appendChild(fullPostElement);
 
 const blogId = getQueryParam("id");
 //აქ მოწმდებბა ის თუკი არ იარსებებს ასეთი id მაში გამოიტანოს კონსოლში ერორი
