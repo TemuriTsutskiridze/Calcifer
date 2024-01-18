bee25c277e035c408b0af41b5366e001c6fec1e8
ReziMi <revaz.mikadze92@gmail.com>
2024-01-18 17:16:53 +0400

    feat: add data validations in js file. styles: add some styles in css

--- addBlog.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const calendarIcon = document.getElementById('calendarIcon');
-    const dateInput = document.getElementById('release');
-
-    calendarIcon.addEventListener('click', function () {
-        // Trigger the click event on the associated date input
-        dateInput.click();
-    });
-});
