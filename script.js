a0e71e710de9ef3228b5ea4e01f099b24421ecfd
tikosaghinadze <teasaghinadze20@gmail.com>
2024-01-19 20:56:03 +0400

    give posts div atribute where is save post categories info

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
-const enterPopUp = document.getElementById('button');
-const logInPopUp = document.getElementById('logInPopUp');
-const removePopUp = document.querySelector('.remove');
-const successPopUp = document.getElementById('successPopUp')
-const enterButton = document.querySelector('.enter');
-const emailInput = document.querySelector('.inputMail');
-const notFoundDiv = document.querySelector('.noMail');
-const agreeButton = document.querySelector('.agree');
-const overlay = document.querySelector('.overlay');
+const enterPopUp = document.getElementById("button");
+const logInPopUp = document.getElementById("logInPopUp");
+const removePopUp = document.querySelector(".remove");
+const successPopUp = document.getElementById("successPopUp");
+const enterButton = document.querySelector(".enter");
+const emailInput = document.querySelector(".inputMail");
+const notFoundDiv = document.querySelector(".noMail");
+const agreeButton = document.querySelector(".agree");
+const overlay = document.querySelector(".overlay");
 
 //ვაჭერთ შესვლის ღილაკს და ჩნდება დალოგინების ფანჯარა
-enterPopUp.addEventListener('click', function() {
-    logInPopUp.style.display='flex';
-    overlay.style.display = 'block';
+enterPopUp.addEventListener("click", function () {
+  logInPopUp.style.display = "flex";
+  overlay.style.display = "block";
 });
 
-removePopUp.addEventListener('click', function(){
-    overlay.style.display = 'none';
-    logInPopUp.style.display = 'none';
-    successPopUp.style.display='none';
-})
+removePopUp.addEventListener("click", function () {
+  overlay.style.display = "none";
+  logInPopUp.style.display = "none";
+  successPopUp.style.display = "none";
+});
 
 // აქ შეგვყავს მეილი და მოწმდება რამდენად ვალიდურია
-enterButton.addEventListener('click', function() {
-    // Regular expression for checking if the email ends with "@redberry.ge"
-    const re = /@redberry\.ge$/;
+enterButton.addEventListener("click", function () {
+  // Regular expression for checking if the email ends with "@redberry.ge"
+  const re = /@redberry\.ge$/;
 
-    if (re.test(emailInput.value.trim())) {
-        logInPopUp.style.display = 'none';
-        successPopUp.style.display='flex';
-    } else {
-        notFoundDiv.style.display = 'flex';
-    }
+  if (re.test(emailInput.value.trim())) {
+    logInPopUp.style.display = "none";
+    successPopUp.style.display = "flex";
+  } else {
+    notFoundDiv.style.display = "flex";
+  }
 });
 
 // ვაწვებით ღილაკს "კარგი" და ვხურავთ ფანჯარას
-agreeButton.addEventListener('click', function(){
-    successPopUp.style.display='none';
-    overlay.style.display = 'none';
-})
-
+agreeButton.addEventListener("click", function () {
+  successPopUp.style.display = "none";
+  overlay.style.display = "none";
+});
 
 async function loadData() {
   const response = await fetch("data.json");
@@ -85,6 +84,10 @@ function addPostToHTML(post) {
 
   const postElement = document.createElement("div");
   postElement.classList.add("blog-post");
+  postElement.setAttribute(
+    "data-categories",
+    post.categories.map((item) => item.name).join("-")
+  );
 
   postElement.innerHTML = `
     <img src="${post.image}" alt="Blog Image" class="post-image">
@@ -112,4 +115,3 @@ function addPostToHTML(post) {
 }
 
 loadData();
-
